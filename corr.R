28275fba4c98643e4c43b4e672c13984569289b3
BanpoSolid <hosung.choi@gmail.com>
2017-07-06 10:15:27 +0900

    Test

---
+++ corr.R
@@ -0,0 +1,6 @@
+corr <- function(directory, threshold = 0) {
+        filepath <- paste(getwd(), "/", directory, sep="")
+        filename <- as.character(list.files(filepath))
+        
+        
+}
